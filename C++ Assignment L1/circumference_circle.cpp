62ca2b9a3b15773ad08b8fb9539935e2b3d8aa74
AmanAlgo1110 <149589970+AmanAlgo1110@users.noreply.github.com>
2024-09-17 19:59:04 +0530

    Solved C++ Assignments of  Lecture 1

---
+++ C++ Assignment L1/circumference_circle.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float radius=10,circumference;
+    circumference = 2*3.14*radius;
+    cout<<circumference;
+    return 0;
+}
---
+++ C++ Assignment L1/print_10.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,z;
+    x=5;
+    y=5;
+    z=x+y;
+    cout<<z;
+    return 0;
+}
